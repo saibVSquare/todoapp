b9ff25d217c03b0ff28ccffcbed7289178279e28
Saib Ali <145130840+saibVSquare@users.noreply.github.com>
2023-09-21 20:05:01 +0500

    some

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,6 +3,9 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 export const Todo = ({ task, deleteTodo, toggleComplete }) => {
+  const func = () => {
+    alert("dd");
+  };
   return (
     <div className="Todo">
       <p
@@ -14,6 +17,7 @@ export const Todo = ({ task, deleteTodo, toggleComplete }) => {
       <div>
         <FontAwesomeIcon icon={faPenToSquare} />
         <FontAwesomeIcon icon={faTrash} onClick={() => deleteTodo(task.id)} />
+        <button onClick={func}>Testing</button>
       </div>
     </div>
   );
